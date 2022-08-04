4b4539d413b395315f8dcbaf23bfaafebfa9e57e
Nathan Englehart <nengleha@oberlin.edu>
2022-08-04 13:31:28 +0000

    driver and figs

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from lib.ols import ols_regression
 from lib.ridge import ridge_regression
 from lib.logit import logit_regression
 
-plt.style.use('seaborn-poster')
+plt.style.use('seaborn-poster')	
 
-if __name__ == '__main__':
+def ols_driver():
 
-	# DRIVER FOR VARIOUS REGRESSION EXAMPLES
+	# DRIVER FOR VARIOUS OLSREGRESSION EXAMPLES
 	# UNCOMMENT plt.show FOR VISUALIZATIONS
 
-	data = pd.read_csv("data/mpg.csv", sep=",")
+	data = pd.read_csv('data/mpg.csv', sep=",")
 	
 	# SIMPLE OLS REGRESSION
 	
@@ -36,6 +36,7 @@ if __name__ == '__main__':
 	plt.plot(np.array(data['weight']), t_hat, color='k')
 	plt.xlabel('weight')
 	plt.ylabel('mpg')
+	plt.savefig('figs/simple_ols.png')
 	plt.show()
 
 	# POLYNOMIAL OLS REGRESSION 
@@ -55,7 +56,9 @@ if __name__ == '__main__':
 	plt.plot(x, t_hat, color='k')
 	plt.xlabel('weight')
 	plt.ylabel('mpg')
+	plt.savefig('figs/polynomial_ols.png')
 	plt.show()
+
 
 	# MULTIVARIATE OLS REGRESSION
 
@@ -82,7 +85,7 @@ if __name__ == '__main__':
 
 	# Graph
 
-	fig = plt.figure(figsize = (1000,1000))
+	fig = plt.figure(figsize = (100,100))
 	ax = plt.axes(projection='3d')
 	ax.plot_surface(x_pairs,y_pairs,z, rstride=1, cstride=1, color='teal', alpha=0.4, antialiased=False)
 	ax.scatter(x,y,t, c = 'r')
@@ -90,6 +93,7 @@ if __name__ == '__main__':
 	ax.set_title('mpg', fontsize=20)
 	plt.xlabel('\n\n\nweight', fontsize=18)
 	plt.ylabel('\n\n\ndisplacement', fontsize=16)
+	plt.savefig('figs/multivariate_ols.png')
 	plt.show()
 
 	# POLYNOMIAL MULTIVARIATE OLS REGRESSION
@@ -120,7 +124,7 @@ if __name__ == '__main__':
 
 	# Graph
 
-	fig = plt.figure(figsize = (1000,1000))
+	fig = plt.figure(figsize = (100,100))
 	ax = plt.axes(projection='3d')
 	ax.plot_surface(x_pairs,y_pairs,z, rstride=1, cstride=1, color='teal', alpha=0.4, antialiased=False)
 	ax.scatter(x,y,t, c = 'r')
@@ -128,4 +132,131 @@ if __name__ == '__main__':
 	ax.set_title('mpg', fontsize=20)
 	plt.xlabel('\n\n\nweight', fontsize=18)
 	plt.ylabel('\n\n\ndisplacement', fontsize=16)
+	plt.savefig('figs/polynomial_multivariate_ols.png')
 	plt.show()
+
+def ridge_driver():
+
+	# DRIVER FOR VARIOUS OLSREGRESSION EXAMPLES
+	# UNCOMMENT plt.show FOR VISUALIZATIONS
+
+	data = pd.read_csv("data/mpg.csv", sep=",")
+	
+	# SIMPLE RIDGE REGRESSION
+	
+	t = np.array(data['mpg'])
+	x = np.array(data['weight'])
+
+	X = np.array([np.ones(len(t)), x]).T
+
+	model = ridge_regression()
+	model = ridge_regression().fit(X,t)
+
+	t_hat = model.predict(X)
+	
+	plt.scatter(np.array(data['weight']), np.array(data['mpg']), color='g')
+	plt.plot(np.array(data['weight']), t_hat, color='k')
+	plt.xlabel('weight')
+	plt.ylabel('mpg')
+	plt.savefig('figs/simple_ridge.png')
+	plt.show()
+
+	# POLYNOMIAL RIDGE REGRESSION 
+	
+	t = np.array(data['mpg'])
+	x = np.array(data['weight'])
+
+	X = np.array([np.ones(len(t)), x, np.square(x)]).T
+
+	model = ridge_regression()
+	model = ridge_regression().fit(X,t)
+
+	t_hat = model.predict(X)
+
+	plt.scatter(x, t, color='g')
+	x, t_hat = zip(*sorted(zip(x,t_hat))) # plot points in order
+	plt.plot(x, t_hat, color='k')
+	plt.xlabel('weight')
+	plt.ylabel('mpg')
+	plt.savefig('figs/polynomial_ridge.png')
+	plt.show()
+
+	# MULTIVARIATE RIDGE REGRESSION
+
+	t = np.array(data['mpg'])
+	x = np.array(data['weight'])
+	y = np.array(data['displacement'])
+
+	X = np.array([np.ones(len(t)), x, y]).T
+
+	model = ridge_regression()
+	model = ridge_regression().fit(X,t)
+
+	t_hat = model.predict(X)
+
+	# Create set of ordered pairs to work with on graph
+
+	x_pts = np.linspace(x.min(), x.max(), 30)
+	y_pts = np.linspace(y.min(), y.max(), 30)
+	x_pairs, y_pairs = np.meshgrid(x_pts,y_pts)
+
+	# Get values for all ordered pairs in set using model
+
+	z = model.coef_[0] + model.coef_[1] * x_pairs + model.coef_[2] * y_pairs
+
+	# Graph
+
+	fig = plt.figure(figsize = (100,100))
+	ax = plt.axes(projection='3d')
+	ax.plot_surface(x_pairs,y_pairs,z, rstride=1, cstride=1, color='teal', alpha=0.4, antialiased=False)
+	ax.scatter(x,y,t, c = 'r')
+	ax.set_ylabel('displacement')
+	ax.set_title('mpg', fontsize=20)
+	plt.xlabel('\n\n\nweight', fontsize=18)
+	plt.ylabel('\n\n\ndisplacement', fontsize=16)
+	plt.savefig('figs/multivariate_ridge.png')
+	plt.show()
+
+	# POLYNOMIAL MULTIVARIATE RIDGE REGRESSION
+
+	t = np.array(data['mpg'])
+	x = np.array(data['weight'])
+	y = np.array(data['displacement'])
+	X = np.array([x, y]).T
+
+	degree = 2
+	poly = PolynomialFeatures(degree)
+	X = poly.fit_transform(X)
+
+	model = ridge_regression()
+	model = ridge_regression().fit(X,t)
+
+	t_hat = model.predict(X)
+
+	# Create set of ordered pairs to work with on graph
+
+	x_pts = np.linspace(x.min(), x.max(), 30)
+	y_pts = np.linspace(y.min(), y.max(), 30)
+	x_pairs, y_pairs = np.meshgrid(x_pts,y_pts)
+
+	# Get values for all ordered pairs in set using model
+
+	z = model.coef_[0] + model.coef_[1] * x_pairs + model.coef_[2] * y_pairs + model.coef_[3] * x_pairs**2 + (model.coef_[4] * y_pairs * x_pairs) + (model.coef_[5] * y_pairs**2)
+
+	# Graph
+
+	fig = plt.figure(figsize = (100,100))
+	ax = plt.axes(projection='3d')
+	ax.plot_surface(x_pairs,y_pairs,z, rstride=1, cstride=1, color='teal', alpha=0.4, antialiased=False)
+	ax.scatter(x,y,t, c = 'r')
+	ax.set_ylabel('displacement')
+	ax.set_title('mpg', fontsize=20)
+	plt.xlabel('\n\n\nweight', fontsize=18)
+	plt.ylabel('\n\n\ndisplacement', fontsize=16)
+	plt.savefig('figs/polynomial_multivariate_ridge.png')
+	plt.show()
+
+if __name__ == '__main__':
+	
+	ols_driver()
+	ridge_driver()
